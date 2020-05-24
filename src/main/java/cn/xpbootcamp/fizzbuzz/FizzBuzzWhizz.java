27d3d2b6ef131f546485223169d5051b5c57801a
guzhongren <guzhongren@live.cn>
2020-05-24 11:53:10 +0800

    refactor Fizz

--- src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzzWhizz.java
+++ src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzzWhizz.java
@@ -4,12 +4,15 @@ import static java.lang.String.valueOf;
 
 public class FizzBuzzWhizz {
     private int num;
+
+    private String FIZZ = "Fizz";
+
     public FizzBuzzWhizz(int num) {
         this.num = num;
     }
 
     public String say() {
-        if(this.num % 3 == 0) return "Fizz";
+        if(this.num % 3 == 0) return this.FIZZ;
         return valueOf(this.num);
     }
 }
