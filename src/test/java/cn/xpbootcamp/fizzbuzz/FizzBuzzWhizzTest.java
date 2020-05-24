7996de954e6680009d9bd74e4a6a82c30bebda9b
guzhongren <guzhongren@live.cn>
2020-05-24 11:40:59 +0800

    init test env

---
+++ src/test/java/cn/xpbootcamp/fizzbuzz/FizzBuzzWhizzTest.java
@@ -0,0 +1,12 @@
+package cn.xpbootcamp.fizzbuzz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzWhizzTest {
+
+    @Test
+    public void canRunTest() {
+        Assertions.assertEquals(42,42);
+    }
+}
