8a9d6bb4119923046994dd6e629aabd2c0be67cc
guzhongren <guzhongren@live.cn>
2020-05-24 11:45:41 +0800

    return 1 when input is 1

---
+++ src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzzWhizz.java
@@ -0,0 +1,10 @@
+package cn.xpbootcamp.fizzbuzz;
+
+public class FizzBuzzWhizz {
+    public FizzBuzzWhizz(int num) {
+    }
+
+    public String say() {
+        return "1";
+    }
+}
--- src/test/java/cn/xpbootcamp/fizzbuzz/FizzBuzzWhizzTest.java
+++ src/test/java/cn/xpbootcamp/fizzbuzz/FizzBuzzWhizzTest.java
@@ -1,12 +1,19 @@
 package cn.xpbootcamp.fizzbuzz;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzWhizzTest {
 
     @Test
     public void canRunTest() {
-        Assertions.assertEquals(42,42);
+        assertEquals(42,42);
+    }
+
+    @Test
+    public void should_return_1_when_say_given_input_is_1() {
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz(1);
+        assertEquals("1", fizzBuzzWhizz.say());
     }
 }
